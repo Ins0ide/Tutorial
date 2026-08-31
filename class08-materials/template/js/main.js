837fc840c0b6a3d1174ffe79260775fcc4db037b
Ins0ide <josephdavidd73@gmail.com>
2026-08-31 18:28:35 +0100

    update

--- class08-materials/template/js/main.js
+++ class08-materials/template/js/main.js
@@ -1,7 +1,17 @@
 alert('Hello people');
 
+// Console Methods
 console.log('This is sad and wonderful');
+console.warn('This is a warning');
+console.error('This is an error');
 
-document.querySelector('section').addEventListener(() => {
+// document.querySelector('section').addEventListener(() => {
 
-});
+// });
+
+// Variables
+let subject = 'bone of contention'
+num = 30
+subject = 'the number one issue'
+
+console.warn(subject);
