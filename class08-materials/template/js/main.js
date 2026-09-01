6796548f820dcfd85535e6f77c1c5b99e63720a5
Ins0ide <josephdavidd73@gmail.com>
2026-09-01 23:14:06 +0100

    update

--- class08-materials/template/js/main.js
+++ class08-materials/template/js/main.js
@@ -15,3 +15,18 @@ num = 30
 subject = 'the number one issue'
 
 console.warn(subject);
+
+
+const todos = [
+    {
+        id : 1,
+        task : "Take out garbage",
+        isCompleted : true,
+    },
+    {
+        id : 2,
+        task : "3 hours prayer stretch",
+        isCompleted : true,
+    }
+]
+console.log(todos[1].task);
