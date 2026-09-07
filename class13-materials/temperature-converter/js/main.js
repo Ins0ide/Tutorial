8119e56bb39437702e8e85ecc36427adb20fa588
Ins0ide <josephdavidd73@gmail.com>
2026-09-07 10:58:17 +0100

    update

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,9 @@
 //Write your pseduo code first! 
+document.querySelector('#btn').addEventListener('click', () => {
+    let input = document.querySelector("#input").value
+
+    const convert = input * 9/5 + 32
+
+    document.querySelector('#result').innerText = convert
+});
+
