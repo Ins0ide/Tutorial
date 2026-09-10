c57d06271a5124ec1af963710f8f5fb905a81cc3
Ins0ide <josephdavidd73@gmail.com>
2026-09-11 00:27:29 +0100

    update

--- class13-materials/simple-functions/js/main.js
+++ class13-materials/simple-functions/js/main.js
@@ -1,5 +1,23 @@
 //create a function that adds two numbers and alerts the sum
+let add = function (num1, num2) {
+    // Use strict not enabled, I am free to assign the variable without declaration
+    addition = num1 + num2
+    alert(addition);
+};
+add(3,6);
 
 //create a function that multiplys three numbers and console logs the product
+let multiply = function (num1, num2, num3) {
+    // Use strict not enabled, I am free to assign the variable without declaration
+    product = num1 * num2 * num3
+    console.log(product);
+};
+multiply(5,7,8);
 
 //create a function that divides two numbers and returns the ???
+let divide = function (num1, num2) {
+    // Use strict not enabled, I am free to assign the variable without declaration
+    division = num1 / num2
+    console.log(division);
+};
+divide(15,3);
