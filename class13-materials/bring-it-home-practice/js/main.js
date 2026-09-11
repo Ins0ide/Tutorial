8c9a62aa57a8eb2b3cb87551591126082e821bf3
Ins0ide <josephdavidd73@gmail.com>
2026-09-11 01:08:28 +0100

    update

--- class13-materials/bring-it-home-practice/js/main.js
+++ class13-materials/bring-it-home-practice/js/main.js
@@ -1,5 +1,7 @@
 // *Variables*
 // Create a variable and console log the value
+let me = "sabi boy"
+console.log(me);
 
 // Create a variable, add 10 to it, and alert the value
 
@@ -12,3 +14,12 @@
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+let multiply = (num1, num2, num3) => {
+    product = num1 * num2 * num3
+    if(product % 3 === 0) {
+        alert("ZEBRA");
+    }else {
+        alert("This number is not divisible by 3");
+    }
+};
+multiply(3,6,7);
